1cb800e11071e3e22496a5789ffbee7fadebb31a
Alina Zare <zarea@missouri.edu>
2018-01-11 14:37:29 -0500

    add Assignment_01 folder

---
+++ Assignment_01/assignment01.py
@@ -0,0 +1,9 @@
+def assignment01():
+
+	''' Assignment 01 for Introduction to Machine Learning, Sp 2018
+		Introduction to Python and Git assignment
+	''' 
+
+
+if __name__ == '__main__':
+	assignment01()
