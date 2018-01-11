7568cfc2768d1eab9b1b52d43494eb96567c1b6f
ishquark13 <ish.boson13@gmail.com>
2018-01-11 14:17:14 -0500

    update assignment0
    testing my branch

--- Assignment_00/assignment0.py
+++ Assignment_00/assignment0.py
@@ -1,3 +1,7 @@
+ish_dict = {'Name': 'Ishmael', 'Level': 'Senior',
+			'Major': 'Electrical Engineering & Math', 'Dept': 'ECE', 'username': 'ishquark13',
+			'learn': 'Neural Networks for augmenting topologies' }
+
 def assignment0():
 
 	''' Assignment 0 for Introduction to Machine Learning, Sp 2018
@@ -6,15 +10,15 @@ def assignment0():
 	 	Submit your assignment as described in class by pushing to github. 
 	''' 
 
-	print('My name is [your name]')
-	print('I am a [your level] majoring in [your major] in the Department of [your department].')
-	print('I have [no/some/extensive] programming experience in general.')
-	print('I have [no/some/extensive] python programming experience.')
-	print('I have [no/some/extensive] experience with using version control software')
-	print('I have [no/some/extensive] experience with git.')
-	print('I have [no/some/extensive] experience in machine learning.')
-	print('My github username is [username].')
-	print('I am excited to learn the following in this class: [your response]')
+	print('My name is ' + ish_dict['Name'])
+	print('I am a ' + ish_dict['Level'] + ' majoring in ' + ish_dict['Major'] +  ' in the Department of ' + ish_dict['Dept'] + '.')
+	print('I have some programming experience in general.')
+	print('I have some python programming experience.')
+	print('I have some experience with using version control software')
+	print('I have some experience with git.')
+	print('I have some experience in machine learning.')
+	print('My github username is ' + ish_dict['username'] + '.')
+	print('I am excited to learn the following in this class: ' +  ish_dict['learn'])
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 	assignment0()
