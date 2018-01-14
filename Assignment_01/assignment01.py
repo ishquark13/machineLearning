c6010aa867331749755551b201915a301d2c1464
ishquark13 <ish.boson13@gmail.com>
2018-01-14 17:10:33 -0500

    replacing assignment01.py with part 2 and delete old report

--- Assignment_01/assignment01.py
+++ Assignment_01/assignment01.py
@@ -1,19 +1,7 @@
-''' Adding this comment as
-a proof that my terminal is able 
-to push to github properly
-'''
-
 import math 
 import numpy as np
 import matplotlib.pyplot as plt
 from pylab import *
-
-def assignment01():
-
-	''' Assignment 01 for Introduction to Machine Learning, Sp 2018
-		Introduction to Python and Git assignment
-	''' 
-
 
 '''
 Git tutorial
@@ -82,6 +70,16 @@ print(np.shape(test_comp))
 
 
 
+ 
+
+###### PART TWO ########
+print(np.amax(test))
+print(np.amin(test))
+
+test_norm2 = (test_sub - np.amin(test)) / np.amax(test)
+print(test_norm2)
+
+##### ADD SUBPLOTS #####
 fig               = plt.figure()
 
 fig.subplots_adjust(hspace=.5)
@@ -92,13 +90,9 @@ plt.subplot(3,1,2)
 plt.title('Mean-subtracted Data')
 plt.scatter(test_sub[:,0], test_sub[:,1])
 plt.subplot(3,1,3)
-plt.title('Normalized Data')
-plt.scatter(test_comp[:,0], test_comp[:,1])
+plt.title('Normalized Data 2')
+plt.scatter(test_norm2[:,0], test_norm2[:,1])
 plt.show()
 
 
-###### PART TWO ########
-
-
-if __name__ == '__main__':
-	assignment01()
+##### FINITO ###########
