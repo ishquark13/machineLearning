e957f54154e4cd8f695a0dd7e8c1bfa93484b9dc
ishquark13 <ish.boson13@gmail.com>
2018-01-22 23:01:44 -0500

    submitting assignment 02

--- Assignment_02/Assignment02.py
+++ Assignment_02/Assignment02.py
@@ -23,6 +23,46 @@ def generateRandData(N, l, u, gVar):
 def fitdata(x,t,M):
 	'''fitdata(x,t,M): Fit a polynomial of order M to the data (x,t)'''	
 	#This needs to be filled in
+	#get w parameters
+	#feature extract
+
+	#allocate memory for array
+	X = np.zeros(shape=(M,M))
+
+	X = np.array([x**m for m in range(M+1)]).T
+	#make sure size match 
+
+
+	w = np.linalg.inv(X.T@X)@X.T@t
+
+	#adding residue
+	#lambda = 0.035
+	#eye    = lambda*(np.identity(M+1))
+	#w = np.linalg.inv(X.T@X+eye)@X.T@t
+	return w
+
+
+def fitdata2(x,t,M,l):
+	'''fitdata(x,t,M): Fit a polynomial of order M to the data (x,t)'''	
+	#This needs to be filled in
+	#get w parameters
+	#feature extract
+
+	#allocate memory for array
+	X = np.zeros(shape=(M,M))
+
+	X = np.array([x**m for m in range(M+1)]).T
+	#make sure size match 
+
+
+	#w = np.linalg.inv(X.T@X)@X.T@t
+
+	#adding residue
+	lambduh = l
+	eye     = lambduh*(np.identity(M+1))
+	w       = np.linalg.inv(X.T@X+eye)@X.T@t
+	return w	
+
 
 def plotPoly(x,t,w,l,u,subplotloc):
 	'''plotPoly(x,t,w,l,u,subplotloc): Plot data (x,t) and the polynomial with parameters w across the range [l,u) in a sub figure at location subplotloc'''
@@ -46,9 +86,135 @@ def plotPoly(x,t,w,l,u,subplotloc):
 	fig.add_subplot(*subplotloc).set_title("\n".join(textwrap.wrap(myTitle, 50)))
 	plt.legend((p1[0],p2[0],p3[0]),('True Function', 'Training Data', 'Estimated\nPolynomial'), fontsize=6)
 
+    
 
 if __name__ == '__main__':
 	fig = plt.figure()
 
 	#This needs to be filled in
+	data_good  = generateRandData(10,-.5,.5,0)
+	data_noise = generateUniformData(10,-.5,.5,0)
+
+	data_total = np.transpose(np.add(data_good, data_noise))
+
+	#print(data_total)
+
+	#size should be 10,2
+
+	#print(np.shape(data_total))
+
+
+
+	x = np.array(data_total[:,0])
+	t = np.array(data_total[:,1])
+
+	xx = np.array(generateUniformData(10,-1,1,.209)).T
+	t1 = np.array(xx[:,1])
+	x1 = np.array(xx[:,0])
+
+	#allocate memory for array
+	#X = np.zeros(shape=(M,M))
+
+	#X = np.array([x**m for m in range(M+1)]).T
+	#make sure size match 
+
+	'''
+	print(np.shape(x))
+	print(np.shape(X))
+	print(X)
+	'''
+
+	#print(np.shape(t))
+	#w = np.linalg.inv(X.T@X)@X.T@t
+	 
+	#print(w)
+	#print(np.shape(w))
+
+
+	 
+
+	#generate new X_test data to see 
+	# try with x and t having noise added or
+	# try with x1 and t1 with randomly generated variance
+
+
+
+#part1
+# test for figs 1.4
+# call 4 times for different M
+w     = fitdata(x1,t1,0) # try with order as last params M
+plotPoly(x1,t1,w,-1,1,[221])
+w     = fitdata(x1,t1,1) # try with order as last params M
+plotPoly(x1,t1,w,-1,1,[222])
+w     = fitdata(x1,t1,3) # try with order as last params M
+plotPoly(x1,t1,w,-1,1,[223])
+w     = fitdata(x1,t1,9) # try with order as last params M
+plotPoly(x1,t1,w,-1,1,[224])
+plt.show()
+
+#part 2
+# number of data points
+xx = np.array(generateUniformData(15,-1,1,.209)).T
+t1 = np.array(xx[:,1])
+x1 = np.array(xx[:,0])
+w     = fitdata(x1,t1,0) # try with order as last params M
+plotPoly(x1,t1,w,-1,1,[211])
+
+xx = np.array(generateUniformData(100,-1,1,.209)).T
+t1 = np.array(xx[:,1])
+x1 = np.array(xx[:,0])
+w     = fitdata(x1,t1,0) # try with order as last params M
+plotPoly(x1,t1,w,-1,1,[212])
+plt.show()
+
+# part 3
+# increase regularization
+xx = np.array(generateUniformData(10,-1,1,.209)).T
+t1 = np.array(xx[:,1])
+x1 = np.array(xx[:,0])
+w_reg = fitdata2(x1,t1,10, 0.0022) #small lambda
+plotPoly(x1,t1,w_reg,-1,1,[211])
+w_reg = fitdata2(x1,t1,10, 1) #large
+plotPoly(x1,t1,w_reg,-1,1,[212])
+plt.show()
+
+# part 4 
+# show Erms
+
+
+M = 9
+
+w = fitdata(x1,t1,9) # try with order as last params M
+#generate RMS vals
+X1 = np.array([x1**m for m in range(M+1)]).T
+y_train = X1@w.T
+
+trainRms = 0
+print(y_train)
+print(t1)
+
+for m in range(t1.size):
+	trainRms = trainRms + ((y_train-t1)**2)
+
+trainRms = np.array(np.sqrt((trainRms/M+1)))
+
+#ensure overlap
+
+xx2 = np.array(generateUniformData(10,-.5,1.5,.209)).T
+x2  = np.array(xx2[:,0])
+
+X2  = np.array([x2**m for m in range(M+1)]).T
+y_test = X2@w.T
+
+testRms = 0
+
+for m in range(t1.size):
+	testRms = testRms + ((y_test-t1)**2)
+
+testRms = np.array(np.sqrt((testRms/M+1)))
+
+plt.plot(trainRms)
+plt.plot(testRms)	
+plt.show()
+
 
